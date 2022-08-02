064c04ff9aa3fac9494e17661f66ecc1d623035e
João Figueiredo <figueiredo1194@gmail.com>
2022-08-02 18:40:25 -0300

    Create 4_grasshoper.py

---
+++ 4_grasshoper.py
@@ -0,0 +1,12 @@
+# kata link
+# https://www.codewars.com/kata/5612e743cab69fec6d000077
+
+'''
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+'''
+
+a = "dev"
+b = "Lab"
+
+name = a + b
