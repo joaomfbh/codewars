f42abdbb1aa94822f1b691f17b89d43e9f4d97c4
João Figueiredo <figueiredo1194@gmail.com>
2022-08-10 14:58:30 -0300

    Update 8_list_include_this.py

--- 8_list_include_this.py
+++ 8_list_include_this.py
@@ -5,6 +5,6 @@
 Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 '''
 
-def include(arr, item):
+def include(array, item):
   return True if item in arr else False
 
