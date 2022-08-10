ea49171d77db5d16e9d7b6347437408a171571aa
João Figueiredo <figueiredo1194@gmail.com>
2022-08-10 14:56:53 -0300

    Update 8_list_include_this.py file

--- 8_list_include_this.py
+++ 8_list_include_this.py
@@ -1,4 +1,4 @@
-#k kata link
+# kata link
 # https://www.codewars.com/kata/545991b4cbae2a5fda000158
 
 '''
@@ -7,8 +7,4 @@ Create a method that accepts a list and an item, and returns true if the item be
 
 def include(arr, item):
   return True if item in arr else False
-
-my_list = sys.argv[1]
-iten = sys.argv[2]
-print(include(my_list, iten))
 
